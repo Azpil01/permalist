35305bbaf92ae6debf6c6ca6c9c68d08424c1fb5
Azpil <eazpil01@gmail.com>
2026-03-12 18:19:24 -0600

    First commit

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mysql from "mysql2/promise"
+import { createRequire } from "node:module";
 
 const app = express();
 const port = 3000;
+
+const require = createRequire(import.meta.url);
+
+let theUser;
+let thePass;
+
+
+try {
+    const local = require("./config.locals.cjs"); //Le decimos donde estan las variables
+    theUser = local.USER; //Las asignamos a las variables globales "theUser"
+    thePass = local.PASSWORD;
+} catch (error) {    
+}
+
+const connection  = await mysql.createConnection({
+  host: "srv1293.hstgr.io", //info proporcionada por hostinger
+  user: theUser, //info proporcionada por hostinger
+  database: "u354636099_test1", //info proporcionada por hostinger
+  password: thePass, //! NO SE SUBE
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,11 +34,21 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  try {
+    const [result, fields] = await connection.query("SELECT * FROM items")
+    console.log(result)
+    console.log("...")
+    console.log(fields)
+  } catch(err){
+    console.log(err);
+  }
+ 
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
+  
 });
 
 app.post("/add", (req, res) => {
